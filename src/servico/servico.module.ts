4f1ac60948c866d67b36c4f18061a6027d7e392d
unknown <larissateixeira998@gmail.com>
2025-07-10 15:59:47 -0300

    fix: chama módulo do usuário

--- src/servico/servico.module.ts
+++ src/servico/servico.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from '../categoria/categoria.module';
+import { UsuarioModule } from '../usuario/usuario.module';
 import { ServicoController } from './controllers/servico.controller';
 import { Servico } from './entities/servico.entity';
 import { ServicoService } from './services/servico.service';
-import { CategoriaModule } from '../categoria/categoria.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Servico]), CategoriaModule],
+  imports: [
+    TypeOrmModule.forFeature([Servico]),
+    CategoriaModule,
+    UsuarioModule,
+  ],
   providers: [ServicoService],
   controllers: [ServicoController],
   exports: [],
